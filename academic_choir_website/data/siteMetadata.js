cbf4b4f6ee25633cdace178482fccf24d7a08137
Łukasz <75632954+Werthis@users.noreply.github.com>
2022-01-25 14:46:43 +0100

    Delete academic_choir_website directory

--- academic_choir_website/data/siteMetadata.js
+++
@@ -1,8 +0,0 @@
-const siteMetadata = {
-  title: "Chór Akademicki Uniwersytetu Mikałaja Kopernika w Toruniu",
-  author: "Łukasz Stawecki",
-  mail: "chor@umk.pl",
-  facebook: "https://www.facebook.com/chor.umk/",
-};
-
-module.exports = siteMetadata;
